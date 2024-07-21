dbbbba7d7f8a9af5b377470539bfbc99a77a54b5
Clark Hensley <clarkhensley@duck.com>
2024-07-21 09:56:39 -0500

    updating HW, plus ruff

--- hw_bot.py
+++ hw_bot.py
@@ -3,12 +3,15 @@ import discord
 from discord.ext import commands
 import asyncio
 import random
+import logging
 
-bot = commands.Bot(command_prefix="")
+intents = discord.Intents.default()
+intents.reactions = True
+bot = commands.Bot(intents=intents, command_prefix="")
+
 
 @bot.event
-async def on_message(message):
-
+async def on_message(msg):
     arrow_list = ["⬆️", "⬇️"]
 
     reactions_path = pathlib.Path(__file__).parent / "reactions.txt"
@@ -16,21 +19,21 @@ async def on_message(message):
         reactions_list = rf.readlines()
 
     if random.randint(0, 1) > 0:
-        await message.add_reaction(random.choice(reactions_list).strip())
+        await msg.add_reaction(random.choice(reactions_list).strip())
     else:
-        await message.add_reaction(random.choice(arrow_list))
+        await msg.add_reaction(random.choice(arrow_list))
 
 
 async def main():
+    discord.utils.setup_logging(leve=logging.INFO, root=False)
 
     # get the token out of the secret toke doc
-    token_path = pathlib.Path(__file__).parent / "token.secret"
-    with open(token_path, "r", encoding="utf-8") as tf:
-        token = tf.readline()
+    with open("Token.txt", "r", encoding="utf-8") as fp:
+        token = fp.read()
 
-    await bot.start(token)
+    async with bot:
+        await bot.start(token)
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
